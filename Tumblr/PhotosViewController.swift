07a3ab743651aa951b2529ae17ee7604945f748d
Bharath D N <bharathmh@gmail.com>
2017-03-30 23:04:14 -0700

    added refresh capability

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -12,6 +12,7 @@ import AFNetworking
 class PhotosViewController:  UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var posts: [NSDictionary] = []
+    let refreshControl = UIRefreshControl()
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -20,33 +21,13 @@ class PhotosViewController:  UIViewController, UITableViewDataSource, UITableVie
         tableView.dataSource = self
         tableView.rowHeight = 240
         
-        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
-        let request = URLRequest(url: url!)
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate:nil,
-            delegateQueue:OperationQueue.main
-        )
         
-        let task : URLSessionDataTask = session.dataTask(
-            with: request as URLRequest,
-            completionHandler: { (data, response, error) in
-                if let data = data {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(
-                        with: data, options:[]) as? NSDictionary {
-                        //print("responseDictionary: \(responseDictionary)")
-                        
-                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
-                        // This is how we get the 'response' field
-                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
-                        
-                        // This is where you will store the returned array of posts in your posts property
-                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
-                        self.tableView.reloadData()
-                    }
-                }
-        });
-        task.resume()
+        refreshControl.addTarget(self, action: #selector(populatePhotos), for: UIControlEvents.valueChanged)
+
+        tableView.insertSubview(refreshControl, at: 0)
+        populatePhotos()
+        
+        
         
         // Do any additional setup after loading the view.
     }
@@ -93,6 +74,37 @@ class PhotosViewController:  UIViewController, UITableViewDataSource, UITableVie
         }
         
         tableView.deselectRow(at: indexPath, animated:true)
+    }
+    
+    func populatePhotos() {
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                        self.refreshControl.endRefreshing()
+                    }
+                }
+        });
+        task.resume()
     }
     
     /*
